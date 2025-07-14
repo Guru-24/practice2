64034f00ef671281c9c650795bf83546ea6aee8f
Guru-24 <guru.g@s.amity.edu>
2025-07-14 09:58:14 +0530

    added a if and else

--- prac21.c
+++ prac21.c
@@ -9,4 +9,20 @@ int main() {
   fgets(line, sizeof(line), stdin);
 for (int i = 0; line[i] != '\0'; ++i) {
 	line[i] = tolower(line[i]);
+if (line[i] == 'a' || line[i] == 'e' || line[i] == 'i' ||
+        line[i] == 'o' || line[i] == 'u') {
+      ++vowels;
+    }
+    else if ((line[i] >= 'a' && line[i] <= 'z')) {
+      ++consonant;
+    }
+
+    else if (line[i] >= '0' && line[i] <= '9') {
+      ++digit;
+    }
+
+    else if (line[i] == ' ') {
+      ++space;
+    }
+}
 
