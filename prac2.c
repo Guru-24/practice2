1bb0e4f6959e9548a3dd2cb5d5b4342d7920ef0f
Guru-24 <guru.g@s.amity.edu>
2025-07-14 09:39:41 +0530

    added a user enter number

--- prac2.c
+++ prac2.c
@@ -3,4 +3,9 @@
 int main() {
     long long num1, num2;
     long long quotient, remainder;
+ printf("Enter first 8-digit number: ");
+    scanf("%lld", &num1);
+
+    printf("Enter second 8-digit number: ");
+    scanf("%lld", &num2);
 
