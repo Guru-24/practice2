de2af3797202eaebfdebbef83f23849230af3aeb
Guru-24 <guru.g@s.amity.edu>
2025-07-14 09:56:18 +0530

    added a for loop

--- prac21.c
+++ prac21.c
@@ -4,3 +4,9 @@ int main() {
 
   char line[150];
   int vowels, consonant, digit, space;
+  vowels = consonant = digit = space = 0;
+  printf("Enter a line of string: ");
+  fgets(line, sizeof(line), stdin);
+for (int i = 0; line[i] != '\0'; ++i) {
+	line[i] = tolower(line[i]);
+
